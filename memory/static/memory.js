56881de09df7406d694edd75a4ae501d04514392
William Guo <wguo30@stuy.edu>
2023-06-11 21:12:41 -0400

    added a restart button

--- memory/static/memory.js
+++ memory/static/memory.js
@@ -9,69 +9,94 @@ let activeTile = null;
 let awaitingEndOfMove = false;
 
 function buildTile(color) {
-	const element = document.createElement("div");
+    const element = document.createElement("div");
 
-	element.classList.add("tile");
-	element.setAttribute("data-color", color);
-	element.setAttribute("data-revealed", "false");
+    element.classList.add("tile");
+    element.setAttribute("data-color", color);
+    element.setAttribute("data-revealed", "false");
 
-	element.addEventListener("click", () => {
-		const revealed = element.getAttribute("data-revealed");
+    element.addEventListener("click", () => {
+        const revealed = element.getAttribute("data-revealed");
 
-		if (
-			awaitingEndOfMove
-			|| revealed === "true"
-			|| element == activeTile
-		) {
-			return;
-		}
+        if (
+            awaitingEndOfMove ||
+            revealed === "true" ||
+            element === activeTile
+        ) {
+            return;
+        }
 
-		// Reveal this color
-		element.style.backgroundColor = color;
+        // Reveal this color
+        element.style.backgroundColor = color;
 
-		if (!activeTile) {
-			activeTile = element;
+        if (!activeTile) {
+            activeTile = element;
 
-			return;
-		}
+            return;
+        }
 
-		const colorToMatch = activeTile.getAttribute("data-color");
+        const colorToMatch = activeTile.getAttribute("data-color");
 
-		if (colorToMatch === color) {
-			element.setAttribute("data-revealed", "true");
-			activeTile.setAttribute("data-revealed", "true");
+        if (colorToMatch === color) {
+            element.setAttribute("data-revealed", "true");
+            activeTile.setAttribute("data-revealed", "true");
 
-			activeTile = null;
-			awaitingEndOfMove = false;
-			revealedCount += 2;
+            activeTile = null;
+            awaitingEndOfMove = false;
+            revealedCount += 2;
 
-			if (revealedCount === tileCount) {
-				alert("Congrats you won! Refresh to start again.");
-			}
+            if (revealedCount === tileCount) {
+                alert("Congrats you won! Refresh to start again.");
+            }
 
-			return;
-		}
+            return;
+        }
 
-		awaitingEndOfMove = true;
+        awaitingEndOfMove = true;
 
-		setTimeout(() => {
-			activeTile.style.backgroundColor = null;
-			element.style.backgroundColor = null;
+        setTimeout(() => {
+            activeTile.style.backgroundColor = null;
+            element.style.backgroundColor = null;
 
-			awaitingEndOfMove = false;
-			activeTile = null;
-		}, 1000);
-	});
+            awaitingEndOfMove = false;
+            activeTile = null;
+        }, 1000);
+    });
 
-	return element;
+    return element;
 }
 
 // Build up tiles
-for (let i = 0; i < tileCount; i++) {
-	const randomIndex = Math.floor(Math.random() * colorsPicklist.length);
-	const color = colorsPicklist[randomIndex];
-	const tile = buildTile(color);
+function buildTiles() {
+    tilesContainer.innerHTML = ""; // Clear existing tiles
 
-	colorsPicklist.splice(randomIndex, 1);
-	tilesContainer.appendChild(tile);
+    for (let i = 0; i < tileCount; i++) {
+        const randomIndex = Math.floor(Math.random() * colorsPicklist.length);
+        const color = colorsPicklist[randomIndex];
+        const tile = buildTile(color);
+
+        colorsPicklist.splice(randomIndex, 1);
+        tilesContainer.appendChild(tile);
+    }
 }
+
+// Restart game
+function restartGame() {
+    revealedCount = 0;
+    activeTile = null;
+    awaitingEndOfMove = false;
+    colorsPicklist.length = 0;
+
+    // Rebuild colorsPicklist
+    colorsPicklist.push(...colors);
+    colorsPicklist.push(...colors);
+
+    buildTiles();
+}
+
+// Attach restartGame function to the restart button
+const restartButton = document.getElementById('restartButton');
+restartButton.addEventListener('click', restartGame);
+
+// Build initial tiles
+buildTiles();
