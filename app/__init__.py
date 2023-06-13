172657a432183d476b2417cc0ace9f0b110318d2
Jayfiul <azizyusha@gmail.com>
2023-06-12 20:41:23 -0400

    making RPS work

--- app/__init__.py
+++ app/__init__.py
@@ -86,9 +86,39 @@ def memory():
 def snake():
     return render_template('snake.html')
 
-@app.route('/rps')
+#rock paper scissors stuff
+results = []
+player_score = 0
+computer_score = 0
+
+@app.route('/rps', methods=['GET', 'POST'])
 def rps():
-    return render_template('rps.html')
+    global player_score, computer_score
+    if request.method == 'POST':
+        player_choice = request.form['choice']
+        computer_choice = random.choice(['rock', 'paper', 'scissors'])
+        result, player_choice_img, computer_choice_img = get_winner(player_choice, computer_choice)
+        results.append((player_choice, computer_choice, result))
+        if result == "You win!":
+            player_score += 1
+        elif result == "Computer wins!":
+            computer_score += 1
+        return render_template('rps.html', result=result, player_choice=player_choice, computer_choice=computer_choice, player_choice_img=player_choice_img, computer_choice_img=computer_choice_img, results=results, player_score=player_score, computer_score=computer_score)
+
+    return render_template('rps.html', results=results, player_score=player_score, computer_score=computer_score)
+
+def get_winner(player_choice, computer_choice):
+    if player_choice == computer_choice:
+        return "It's a tie!", f"{player_choice}.jpg", f"{computer_choice}.jpg"
+    elif (
+        (player_choice == 'rock' and computer_choice == 'scissors') or
+        (player_choice == 'paper' and computer_choice == 'rock') or
+        (player_choice == 'scissors' and computer_choice == 'paper')
+    ):
+        return "You win!", f"{player_choice}.jpg", f"{computer_choice}.jpg"
+    else:
+        return "Computer wins!", f"{player_choice}.jpg", f"{computer_choice}.jpg"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
